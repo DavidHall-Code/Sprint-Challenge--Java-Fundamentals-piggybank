2ce0efa2b7f7a15896cd6b4fb4bbcd86dfe5b50a
DavidHall-Code <david_m_hall@outlook.com>
2021-02-12 10:33:32 -0500

    comented out the setters as they were not being used

--- src/main/java/com/lambdaschool/piggybank/models/Coin.java
+++ src/main/java/com/lambdaschool/piggybank/models/Coin.java
@@ -8,7 +8,7 @@ public class Coin
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int coinId;
+//    private int coinId;
     private int quantity;
     private double value;
     private String name;
@@ -27,57 +27,57 @@ public class Coin
         //required by jpa
     }
 
-    public int getCoinId()
-    {
-        return coinId;
-    }
+//    public int getCoinId()
+//    {
+//        return coinId;
+//    }
 
     public int getQuantity()
     {
         return quantity;
     }
 
-    public void setQuantity(int quantity)
-    {
-        this.quantity = quantity;
-    }
+//    public void setQuantity(int quantity)
+//    {
+//        this.quantity = quantity;
+//    }
 
     public double getValue()
     {
         return value;
     }
 
-    public void setValue(double value)
-    {
-        this.value = value;
-    }
+//    public void setValue(double value)
+//    {
+//        this.value = value;
+//    }
 
     public String getName()
     {
         return name;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
+//    public void setName(String name)
+//    {
+//        this.name = name;
+//    }
 
     public String getNameplural()
     {
         return nameplural;
     }
 
-    public void setNameplural(String nameplural)
-    {
-        this.nameplural = nameplural;
-    }
+//    public void setNameplural(String nameplural)
+//    {
+//        this.nameplural = nameplural;
+//    }
 
 
     @Override
     public String toString()
     {
         return "Coin{" +
-                "coinId=" + coinId +
+//                "coinId=" + coinId +
                 ", quantity=" + quantity +
                 ", value=" + value +
                 ", name='" + name + '\'' +
