421943ac24e5144355c02f3fe4724797aeca408e
DavidHall-Code <david_m_hall@outlook.com>
2021-03-12 11:22:13 -0500

    Set up coin controller

---
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -0,0 +1,46 @@
+package com.lambdaschool.piggybank.controllers;
+
+import com.lambdaschool.piggybank.models.Coin;
+import com.lambdaschool.piggybank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CoinController
+{
+    @Autowired
+    private CoinRepository coinrepos;
+
+    // http://localhost:2019/total
+    @GetMapping(value = "/total", produces = {"application/json"})
+    public ResponseEntity<?> listCoinTotal()
+    {
+        List<Coin> myList = new ArrayList<>();
+        coinrepos.findAll()
+                .iterator()
+                .forEachRemaining(myList::add);
+
+        double total = 0.0;
+        for (Coin c : myList)
+        {
+            total += c.getValue() * c.getQuantity()
+                    if (c.getQuantity() == 1)
+                    {
+                        System.out.println(c.getQuantity() + " " + c.getName());
+                    } else
+                    {
+                        System.out.println(c.getQuantity() + " " + c.getNameplural());
+                    }
+        }
+        System.out.println("Your bank has: " + total);
+        System.out.println();
+
+        return new ResponseEntity<>(HttpStatus.OK)
+    }
+} // CoinController End
